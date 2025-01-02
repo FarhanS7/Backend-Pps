d2ad8702b4db2ca0fb912627f5c473a7129be40b
Farhan Shahriar <163057878+FarhanS7@users.noreply.github.com>
2025-01-02 23:40:43 +0600

    Delete Github-Repo-Downloader directory

--- Github-Repo-Downloader/app/config/config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5000;
-export const WEB_CACHE = false;
-export const MAX_JSON_SIZE = "10MB";
-export const URL_ENCODE = true;
-export const REQUEST_TIME = 20 * 60 * 1000;
-export const REQUEST_NUMBER = 2000;
--- Github-Repo-Downloader/routes/api.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import * as GitController from "../app/controllers/GitController.js";
-const router = express.Router();
-
-router.get("/:owner/:repo", GitController.downloadRepo);
-
-export default router;
